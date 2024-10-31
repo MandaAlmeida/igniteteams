75316451cf59def2269d9723357bd667ceea669c
amanda almeida <manda.fonsecaff@gmail.com>
2024-10-31 20:26:40 -0300

    Adiciona função para adicionar os players nos times

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/native";
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 
 import { Header } from "@/components/Header";
 import { Highlight } from "@/components/Highlight";
@@ -12,63 +12,90 @@ import { ListEmpty } from "@/components/ListEmpty/index.";
 import { Button } from "@/components/Button";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { AppError } from "@/utils/AppError";
+import { playerAddByGroup } from "@/storage/players/playerAddByGroup";
+import { playersGetByGroup } from "@/storage/players/playersGetByGroup";
 
 type RouteParams = {
     group: string
 }
 
 export default function Players() {
+    const [newPlayerName, setNewPlayerName] = useState("")
     const [team, setTeam] = useState("Time A");
     const [players, setPlayers] = useState(["Jogador 1"]);
 
     const route = useRoute();
     const { group } = route.params as RouteParams;
 
-    return(
-        <Container>
-            <Header showBackButton/>
+    async function handleAddPlayer() {
+        if (newPlayerName.trim().length === 0) {
+            return Alert.alert("Nova pessoa", "Informe nome da pessoa para adicionar");
+        }
 
-            <Highlight title={group} subtitle="Adicione a galera e separe os times"/>
+        const newPlayer = {
+            name: newPlayerName,
+            team,
+        }
+
+        try {
+            await playerAddByGroup(newPlayer, group);
+            const players = await playersGetByGroup(group);
+        } catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert('Nova pessoa', error.message)
+            } else {
+                console.log(error);
+                Alert.alert('Nova pessoa', 'Não foi possivel adicionar')
+            }
+        }
+    }
+
+    return (
+        <Container>
+            <Header showBackButton />
+
+            <Highlight title={group} subtitle="Adicione a galera e separe os times" />
 
             <Form>
-            <Input placeholder="Nome da pessoa" autoCorrect={false}/>
-            <ButtonIcon icon="add"/>
+                <Input placeholder="Nome da pessoa" autoCorrect={false} onChangeText={setNewPlayerName} />
+                <ButtonIcon icon="add" onPress={handleAddPlayer} />
             </Form>
             <HeaderList>
                 <FlatList
-                 data={["Time A", "Time B"]}
-                keyExtractor={item => item}
-                renderItem={({item}) => (
-                <Filter 
-                title={item} 
-                isActive={item === team}
-                onPress={() => setTeam(item)}
-                />
-                )}
-                horizontal
+                    data={["Time A", "Time B"]}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
                 />
                 <NumberOfPlayers>
-                {players.length}
+                    {players.length}
                 </NumberOfPlayers>
             </HeaderList>
-           <FlatList 
-           data={players}
-           keyExtractor={item => item}
-                renderItem={({item}) => (
-                    <PlayerCard name={item} onRemove={() => {}}/>
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <PlayerCard name={item} onRemove={() => { }} />
                 )}
                 ListEmptyComponent={() => (
-                    <ListEmpty message="Não há pessoas nesse time."/>
+                    <ListEmpty message="Não há pessoas nesse time." />
                 )}
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={[{ paddingBottom: 100},
-                    players.length === 0 && {flex: 1}
+                contentContainerStyle={[{ paddingBottom: 100 },
+                players.length === 0 && { flex: 1 }
                 ]}
-           />
-           <Button
-            title="Remover Turma"
-            type="SECONDARY"
-           />
+            />
+            <Button
+                title="Remover Turma"
+                type="SECONDARY"
+            />
         </Container>
     )
 }
